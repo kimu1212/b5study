83b0f36d1cb1d46386a01788ea4347cb459c9a0f
木村拓也 <kimuratakuya@Mac.local>
2018-05-27 11:26:59 +0900

    テストコード追加

--- src/test/java/com/example/b5study/B5studyApplicationTests.java
+++ src/test/java/com/example/b5study/B5studyApplicationTests.java
@@ -1,16 +1,36 @@
 package com.example.b5study;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class B5studyApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    private MockMvc mvc;
 
+    @Before
+    public void before() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new B5studyApplication()).build();
+    }
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void indexTest() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("Hello World!"));
+    }
 }
